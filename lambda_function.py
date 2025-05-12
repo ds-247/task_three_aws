89a9c89048dd9daf270e97dce7c97af4b6386af5
Cipher <dipanshusaini69@gmail.com>
2025-05-13 00:25:42 +0530

    * successfully implemented glue job and make it running for csv file type * adjusted lambda function for sending variables to glue * adjuste template for creating output glue s3 bucket, glue role and glue job

--- lambda_function.py
+++ lambda_function.py
@@ -7,6 +7,7 @@ dynamodb = boto3.resource('dynamodb')
 glue = boto3.client('glue')
 
 TABLE_NAME = os.environ['DYNAMO_TABLE_NAME'] 
+OUTPUT_STORE_PATH = os.environ['GLUE_OUTPUT_LOCATION']
 
 def lambda_handler(event, context):
    sqs_record = event['Records'][0]
@@ -16,7 +17,7 @@ def lambda_handler(event, context):
    s3_record = s3_event['Records'][0]
    bucket = s3_record['s3']['bucket']['name']
    key = s3_record['s3']['object']['key']
-   size = 1 + s3_record['s3']['object']['size'] / (1024 * 1024)  # Convert bytes to MB
+   size = s3_record['s3']['object']['size'] / (1024 * 1024)  # Convert bytes to MB
    extension = key.split('.')[-1]
    
    print(f"Bucket: {bucket}, Key: {key}, Size: {size} MB, Extension: {extension}")
@@ -30,12 +31,21 @@ def lambda_handler(event, context):
    
    rule = response['Item']
 
+   print("input file path -> ",f"s3://{bucket}/{key}")
 
    if rule['min_size_mb'] <= size <= rule['max_size_mb']:
       job_name = rule['glue_job_name']
       
       # 4. Trigger Glue job
-      # glue.start_job_run(JobName=job_name)
+
+      job_arguments = {
+         "--input_path": f"s3://{bucket}/{key}",
+         "--output_path": f"{OUTPUT_STORE_PATH}",
+         "--file_size": str(size),
+         "--file_extension": extension
+      }
+
+      glue.start_job_run(JobName=job_name, Arguments=job_arguments)
       print(f"Triggered Glue job: {job_name}")
    else:
       print(f"File size {size}MB out of bounds for {extension}")
--- src.py
+++ src.py
@@ -4,37 +4,48 @@ import os
 from botocore.exceptions import ClientError
 
 LAMBDA_BUCKET_NAME = 'lambda-read-config-from-dynamo'
-REGION_NAME = 'ap-south-1'
-ZIP_FILE_NAME = 'lambda_function.zip'
-LAMBDA_HANDLER_FILE_PATH= 'lambda_function.py'
+GLUE_JOB_BUCKET_NAME = 'glue-job-bucket-12-05'
 DATA_BUCKET_NAME = 'data-dump-bucket-11-05'
+GLUE_OUTPUT_BUCKET_NAME = 'glue-output-bucket-11-05'
 TASK_QUEUE_NAME = 'task-queue'
-STACK_NAME = 'task-three-stack-test-7'
+DYNAMO_TABLE_NAME = 'GlueJobRules'
+INFRA_STACK_NAME = 'InfrastructureCreation'
+REGION_NAME = 'ap-south-1'
+LAMBDA_ZIP_FILE_NAME = 'lambda_function.zip'
+CSV_GLUE_FILE_PATH = 'word_count.py'
+LAMBDA_HANDLER_FILE_PATH= 'lambda_function.py'
 INFRA_TEMPLATE_PATH = 'infra_template.yaml'
 LAMBDA_RUNTIME_VERSION = 'python3.12'
-LAMBDA_HANDLER = 'lambda_function.lambda_handler'
-DYNAMO_TABLE_NAME = 'GlueJobRules'
+TEMP_VAR_LAMBDA_HANDLER = 'lambda_function.lambda_handler'
+TEMP_VAR_CSV_GLUE_JOB_NAME = 'CsvParserJob'
+TEMP_VAR_CSV_GLUE_JOB_S3_KEY = CSV_GLUE_FILE_PATH
+TEMP_VAR_CSV_GLUE_SCRIPT_NAME=f's3://{GLUE_JOB_BUCKET_NAME}/{TEMP_VAR_CSV_GLUE_JOB_S3_KEY}'
+TEMP_VAR_CSV_GLUE_OUTPUT_PATH = f's3://{GLUE_OUTPUT_BUCKET_NAME}/csv_output/'
 
 Parameters=[
         {'ParameterKey': 'S3DataBucketName', 'ParameterValue': DATA_BUCKET_NAME},
         {'ParameterKey': 'TaskQueueName', 'ParameterValue': TASK_QUEUE_NAME},
         {'ParameterKey': 'LambdaRuntimeVersion', 'ParameterValue': LAMBDA_RUNTIME_VERSION},
-        {'ParameterKey': 'LambdaHandler', 'ParameterValue': LAMBDA_HANDLER},
+        {'ParameterKey': 'LambdaHandler', 'ParameterValue': TEMP_VAR_LAMBDA_HANDLER},
         {'ParameterKey': 'LambdaBucketName', 'ParameterValue': LAMBDA_BUCKET_NAME},
-        {'ParameterKey': 'LamdbaBucketKey', 'ParameterValue': ZIP_FILE_NAME},
+        {'ParameterKey': 'LambdaBucketKey', 'ParameterValue': LAMBDA_ZIP_FILE_NAME},
         {'ParameterKey': 'DynamoDBTableName', 'ParameterValue': DYNAMO_TABLE_NAME},
+        {'ParameterKey': 'CSVGlueJobName', 'ParameterValue': TEMP_VAR_CSV_GLUE_JOB_NAME},
+        {'ParameterKey': 'CSVGlueScriptLocation', 'ParameterValue': TEMP_VAR_CSV_GLUE_SCRIPT_NAME},
+        {'ParameterKey': 'GlueOutputS3BucketName', 'ParameterValue': GLUE_OUTPUT_BUCKET_NAME},
+        {'ParameterKey': 'CSVGlueOutputLocation', 'ParameterValue': TEMP_VAR_CSV_GLUE_OUTPUT_PATH},
 ]
 
 dynamo_db_config_items = [
         {
             'file_type': 'csv',
-            'min_size_mb': 1,
+            'min_size_mb': 0,
             'max_size_mb': 100,
             'glue_job_name': 'CsvParserJob'
         },
         {
             'file_type': 'json',
-            'min_size_mb': 5,
+            'min_size_mb': 0,
             'max_size_mb': 50,
             'glue_job_name': 'JsonProcessorJob'
         }
@@ -56,27 +67,30 @@ def create_bucket(s3_client, bucket_name, region):
 
 def zip_lambda_function():
     try:
-        with zipfile.ZipFile(ZIP_FILE_NAME, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        if os.path.exists(LAMBDA_ZIP_FILE_NAME):
+            os.remove(LAMBDA_ZIP_FILE_NAME)
+            print(f"Deleted local zip file: {LAMBDA_ZIP_FILE_NAME}")
+
+        with zipfile.ZipFile(LAMBDA_ZIP_FILE_NAME, 'w', zipfile.ZIP_DEFLATED) as zipf:
             zipf.write(LAMBDA_HANDLER_FILE_PATH)
-        print(f"Zipped {LAMBDA_HANDLER_FILE_PATH} to {ZIP_FILE_NAME}")
+        print(f"Zipped {LAMBDA_HANDLER_FILE_PATH} to {LAMBDA_ZIP_FILE_NAME}")
         return True
     except Exception as e:
         print(f"Error zipping {LAMBDA_HANDLER_FILE_PATH}: {e}")
         return False
 
 
-def upload_to_s3(s3_client, bucket_name, file_path):
+def upload_to_s3(s3_client, bucket_name, file_path, s3_key=None):
+    if s3_key is None:
+        s3_key = file_path
+
     try:
-        s3_client.upload_file(file_path, bucket_name, file_path)
-        print(f"Uploaded {file_path} to s3://{bucket_name}/{file_path}")
+        s3_client.upload_file(file_path, bucket_name, s3_key)
+        print(f"Uploaded {file_path} to s3://{bucket_name}/{s3_key}")
+        return True
     except Exception as e:
         print(f"Error uploading {file_path} to s3://{bucket_name}: {e}")
         return False
-    finally:
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"Deleted local zip file: {file_path}")
-            return True
 
 
 def create_infrastructure(cf_client, stack_name, template_body, parameters):
@@ -88,6 +102,12 @@ def create_infrastructure(cf_client, stack_name, template_body, parameters):
             Capabilities=['CAPABILITY_IAM'],
         )
         print(f"Stack {stack_name} creation initiated: {response}")
+        stack_id = response['StackId']
+         # Wait for stack creation to complete (or fail)
+        waiter = cf_client.get_waiter('stack_create_complete')
+        print(f"Waiting for stack '{stack_name}' to complete...")
+        waiter.wait(StackName=stack_id)  # Use the Stack ID
+        print(f"Stack '{stack_name}' creation completed.")
         return True
     except Exception as e:
         print(f"Error creating stack {stack_name}: {e}")
@@ -96,12 +116,6 @@ def create_infrastructure(cf_client, stack_name, template_body, parameters):
 
 def init_dynamo_db(dynamodb_client, table_name, config_items):
     try:
-        # Wait until the table exists
-        waiter = dynamodb_client.meta.client.get_waiter('table_exists')
-        print(f"Waiting for table '{table_name}' to be created...")
-        waiter.wait(TableName=table_name)
-        print(f"Table '{table_name}' has been created.")
-
         table = dynamodb_client.Table(table_name)
 
         for item in config_items:
@@ -120,19 +134,25 @@ def main() :
 
     if not create_bucket(s3, LAMBDA_BUCKET_NAME, REGION_NAME):
         return
+
+    if not create_bucket(s3, GLUE_JOB_BUCKET_NAME, REGION_NAME):
+        return
     
     if not zip_lambda_function():
         return
 
-    if not upload_to_s3(s3, LAMBDA_BUCKET_NAME, ZIP_FILE_NAME):
+    if not upload_to_s3(s3, LAMBDA_BUCKET_NAME, LAMBDA_ZIP_FILE_NAME):
+        return
+    
+    if not upload_to_s3(s3, GLUE_JOB_BUCKET_NAME, CSV_GLUE_FILE_PATH, TEMP_VAR_CSV_GLUE_JOB_S3_KEY):
         return
 
     with open(INFRA_TEMPLATE_PATH, 'r') as f:
         template_body = f.read()
 
-    if not create_infrastructure(cf, STACK_NAME, template_body, Parameters):
+    if not create_infrastructure(cf, INFRA_STACK_NAME, template_body, Parameters):
         return
-    
+
     init_dynamo_db(dynamodb, DYNAMO_TABLE_NAME, dynamo_db_config_items)
 
 
