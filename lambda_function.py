ed41d2f6700008607ee084b57b04e85a76781d8e
Cipher <dipanshusaini69@gmail.com>
2025-05-12 09:51:01 +0530

    * Lambda function configured in order to get right glue job from dynamoDB

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,41 @@
+import boto3
+import os
+import json
+
+s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+glue = boto3.client('glue')
+
+TABLE_NAME = os.environ['DYNAMO_TABLE_NAME'] 
 
 def lambda_handler(event, context):
-   print(event)
+   sqs_record = event['Records'][0]
+   s3_event_str = sqs_record['body']
+   s3_event = json.loads(s3_event_str)
+   
+   s3_record = s3_event['Records'][0]
+   bucket = s3_record['s3']['bucket']['name']
+   key = s3_record['s3']['object']['key']
+   size = 1 + s3_record['s3']['object']['size'] / (1024 * 1024)  # Convert bytes to MB
+   extension = key.split('.')[-1]
+   
+   print(f"Bucket: {bucket}, Key: {key}, Size: {size} MB, Extension: {extension}")
+
+   table = dynamodb.Table(TABLE_NAME)
+   response = table.get_item(Key={'file_type': extension})
+   
+   if 'Item' not in response:
+      print(f"No rule found for {extension}")
+      return
+   
+   rule = response['Item']
+
+
+   if rule['min_size_mb'] <= size <= rule['max_size_mb']:
+      job_name = rule['glue_job_name']
+      
+      # 4. Trigger Glue job
+      # glue.start_job_run(JobName=job_name)
+      print(f"Triggered Glue job: {job_name}")
+   else:
+      print(f"File size {size}MB out of bounds for {extension}")
